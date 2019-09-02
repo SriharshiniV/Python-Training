c5ae428467fa7f53a45d8727b576ff3d93afbc3d
Sriharshini Vallabhaneni <40619600+SriharshiniV@users.noreply.github.com>
2019-09-02 14:08:57 -0500

    Add files via upload

---
+++ list_dic.py
@@ -0,0 +1,22 @@
+list=[1,2,3,4,5]
+
+list.pop()
+print(list)
+
+list.pop(1)
+print(list)
+
+list.append(5)
+print(list)
+
+list.insert(1,2)
+print(list)
+
+del list
+print(list)
+
+dic={'name':'sri','phn':123,'name':'harsh'}
+print(dic)
+
+print(dic['name'])
+
