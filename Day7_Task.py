d569e1212727fd85bd4112162da3e39ce69d9f1a
Sriharshini Vallabhaneni <40619600+SriharshiniV@users.noreply.github.com>
2019-09-04 09:39:19 -0500

    Add files via upload

---
+++ Day7_Task.py
@@ -0,0 +1,12 @@
+class harshini:
+    def sri(self):
+        print("i am a trainee")
+    def harsh(self):
+        print("i am a student")
+
+
+obj1=harshini()
+obj1.sri()
+
+obj2=harshini()
+obj2.harsh()
